5440fbfbc9dec810b2b2b57f4c08190e61fe606c
Erik Odenman <erik.odenman@gmail.com>
2014-02-25 14:00:57 +0100

    Renamed classes

--- Main.java
+++ Main.java
@@ -3,12 +3,12 @@ import java.io.*;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
-		new SyntaxChecker(System.in);
-		List<Token> tokens = SyntaxChecker.parseTokens();
+		new Lexer(System.in);
+		List<Token> tokens = Lexer.parseTokens();
 		System.out.println();
 		System.out.println("START TOKENS");
 		for (Token t : tokens) {
-			System.out.println(t);
+			System.out.println(t + " (" + t.kind + ")");
 		}
 		System.out.println("END TOKENS");
 	}
