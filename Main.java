ad5f3f0f385e6b71c69ef3cf13b28ed8a5d6e4b6
Erik Odenman <erik.odenman@gmail.com>
2014-03-06 15:05:33 +0100

    Added jjTree parser

--- Main.java
+++ Main.java
@@ -1,25 +1,9 @@
-import java.util.*;
-import parser.*;
-import java.io.*;
 import lexer.*;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		new Lexer(System.in);
-		List<Token> tokens = Lexer.parseTokens();
-		System.out.println();
-		System.out.println("START TOKENS");
-		for (Token t : tokens) {
-			System.out.println(t + " (" + t.kind + ")");
-		}
-		System.out.println("END TOKENS");
-		
-		Parser parser = new Parser(tokens);
-		try {
-			parser.parse();
-		} catch (RuntimeException e) {
-			System.err.println("Syntax error");
-			e.printStackTrace();
-		}
+	public static void main(String[] args) throws ParseException {
+        new Lexer(System.in);
+        SimpleNode node = Lexer.Program();
+        node.dump("");
 	}
 }
