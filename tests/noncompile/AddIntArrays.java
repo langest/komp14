12f5a3dcb407cb48e254c2437b45334824150e5e
Erik Odenman <erik.odenman@gmail.com>
2014-04-01 11:19:58 +0200

    Added tests

---
+++ tests/noncompile/AddIntArrays.java
@@ -0,0 +1,10 @@
+class Main {
+    public static void main(String[] args) {
+        int[] a1;
+        int[] a2;
+        int[] a3;
+        a1 = new int[2];
+        a2 = new int[3];
+        a3 = a1 + a2;
+    }
+}
---
+++ tests/noncompile/NonExistingVariable2.java
@@ -0,0 +1,7 @@
+class Main {
+    public static void main(String[] args) {
+        int a;
+        a = 2;
+        System.out.println(a2);
+    }
+}
