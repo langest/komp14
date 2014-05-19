a334389a91b8d44ba6778660a4afb99f5d188fd4
Daniel Langesten <daniel.langest@gmail.com>
2014-05-19 12:42:16 +0200

    added test

---
+++ tests/execute/cpm2.java
@@ -0,0 +1,19 @@
+class Cmp2 {
+	public static main(String[] args) {
+		boolean b1;
+		boolean b2;
+		boolean b3;
+		int i1;
+		int i2;
+		b1 = true;
+		b2 = false;
+		i1 = 1;
+		i2 = 2;
+
+		if (b1) {} else if(b2) {}
+		if (i1 == i2) System.out.println(4); else if (i1!=i2) if (i1 <= i1) if (i2 >= i1) System.out.println(0) else {} else{}  else{}
+		i1 = 2;
+		if (i1 >= i2) if (i1 <= i2) System.out.println(0) else{} else{}
+		System.out.println(7);
+	}
+}
